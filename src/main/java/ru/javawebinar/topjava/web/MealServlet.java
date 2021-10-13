c56e4a5ca8b2bde16131eb08bf104ddefdd0a96d
jimsby <jimsby@gmail.com>
2021-10-13 23:17:17 +0300

    HW1: List of Meals without Color

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,12 +1,16 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.dao.MealDAO;
+import ru.javawebinar.topjava.model.Meal;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -15,6 +19,9 @@ public class MealServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         log.debug("redirect to meals");
-        resp.sendRedirect("meals.jsp");
+        List<Meal> list = MealDAO.listAllMeals();
+        req.setAttribute("listMeals", list);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("meals.jsp");
+        dispatcher.forward(req, resp);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -5,12 +5,30 @@
   Time: 22:16
   To change this template use File | Settings | File Templates.
 --%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <title>Meals</title>
 </head>
 <body>
-Meals
+<div align="center">
+    <table border="1" cellpadding="5">
+        <caption><h2>List of Meals</h2></caption>
+        <tr>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Calories</th>
+        </tr>
+        <jsp:useBean id="listMeals" scope="request" type="java.util.List"/>
+        <c:forEach var="meals" items="${listMeals}">
+            <tr>
+                <td><c:out value="${meals.getDate()}"/></td>
+                <td><c:out value="${meals.getDescription()}"/></td>
+                <td><c:out value="${meals.getCalories()}"/></td>
+            </tr>
+        </c:forEach>
+    </table>
+</div>
 </body>
 </html>
