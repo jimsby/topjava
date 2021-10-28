6ce9a4cf98c22ebead6f7af5e01099cb950c29e9
jimsby <jimsby@gmail.com>
2021-10-28 20:01:08 +0300

    HW3_1 SQL DB Meals created and index added

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
@@ -23,3 +24,15 @@ CREATE TABLE user_roles
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+    meal_id         INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    datetime        TIMESTAMPTZ                         NOT NULL UNIQUE,
+    description     VARCHAR                             NOT NULL,
+    calories        INTEGER                             NOT NULL,
+    user_id         INTEGER                             NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE UNIQUE INDEX user_id_and_datetime_in_meal_idx ON meals (user_id, datetime);
